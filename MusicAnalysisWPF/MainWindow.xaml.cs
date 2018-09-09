f02ea2c3ecc14a8db373358c6f25f1f2adedeb3b
Ognjen <oggy22@gmail.com>
2018-09-09 00:41:01 -0700

    Closes #32

---
+++ MusicAnalysisWPF/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Win32;
+using MusicCore;
+using System.Windows;
+
+namespace MusicAnalysisWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Midi Files|*.mid";
+            openFileDialog.Title = "Select a Midi File";
+
+            if (openFileDialog.ShowDialog().Value)
+            {
+                string filename = openFileDialog.FileName;
+
+                txt.Text += $"File {filename} loading...\r\n";
+                var lists = MidiFileReader.Read(filename);
+                txt.Text += "File loaded.\r\n";
+
+                txt.Text += "Analysing...\r\n";
+                ChomskyGrammarAnalysis.Reduce(lists);
+                txt.Text += $"Analysis finished!\r\n";
+            }
+        }
+    }
+}
